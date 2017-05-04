bbcddeb26bd58e97eab5810b06788c6d519ec906
paszin <pascalslogin@gmail.com>
2017-05-04 14:46:34 +0200

    local server code

---
+++ local_device/gactions_response_builder.py
@@ -0,0 +1,51 @@
+
+## Resources: https://developers.google.com/actions/reference/conversation#http-response
+
+
+class GResponse:
+
+    def __init__(self, conversation_token="", \
+                 expect_user_response=False, \
+                 text="", \
+                 no_input_prompts=["Hmm Sorry, my fault. Try something else"]):
+        self.conversation_token = conversation_token
+        self.expect_user_response = bool(expect_user_response)
+        self.text_to_speech = text
+        self.no_input_prompts = no_input_prompts
+
+    def _build_response(self):
+        resp = {"conversation_token": self.conversation_token,\
+                "expect_user_response": self.expect_user_response}
+        if self.expect_user_response == False:
+            resp["final_response"] = {"speech_response": \
+                                      {"text_to_speech": self.text_to_speech}}
+        else:
+            resp["expected_inputs"] = [\
+                {"input_prompt": {}, \
+                 "possible_intents": [{"intent": "assistant.intent.action.TEXT"}]\
+                 }]
+            
+            resp["expected_inputs"][0]["input_prompt"] = {
+                        "initial_prompts": [
+                          {"text_to_speech": self.text_to_speech}
+                                            ],
+                         "no_input_prompts" : [
+                  {"text_to_speech": s} for s in self.no_input_prompts]
+                        }
+          
+        return resp
+        
+
+    def __repr__(self):
+                                
+        return str(self._build_response())
+
+    def getJson(self):
+        return self._build_response()
+
+
+
+if __name__ == "__main__":
+
+    r = GResponse(expect_user_response=True)
+    print r
--- local_device/simple_server.py
+++ local_device/simple_server.py
@@ -4,14 +4,56 @@
 ## but for the beginning you can use this template
 
 
-from flask import Flask
+
+
+import json
+import random
+import requests
+from flask import Flask, request, jsonify
+
+from gactions_response_builder import GResponse
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    print "incoming request"
-    ## implement your logic here
-    return "Hello World!"
+
+welcomeResponse = GResponse(text="Welcome, do you want some light", expect_user_response=True).getJson()
+okayResponse = GResponse(text="Okay", expect_user_response=False).getJson()
+
+
+def forward_request(text):
+    print "forward"
+    if "turn off" in text:
+        return turnOff()
+        
+    url = "http://192.168.178.93:9000/nlp/effect"
+    return requests.post(url, \
+                     headers = {'content-type': 'application/json'}, \
+                     data=json.dumps({"text": text}))
+    
+
+def turnOff():
+    url = "http://192.168.178.93:9000/reset"
+    return requests.post(url, headers = {'content-type': 'application/json'})
+    
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    print "incoming request:"
+    print request
+    if request.method == "POST":
+        print "Content:"
+        print request.data
+        data = json.loads(request.data)
+        text = data["inputs"][0]["raw_inputs"][0]["query"]
+        print "Extracted Text:", text
+        if data["inputs"][0]["intent"] == "assistant.intent.action.MAIN":
+            print "initial request"
+            return jsonify(welcomeResponse)
+        else:
+            forward_request(text)
+            return jsonify(okayResponse)
+    #return jsonify(GResponse(text="reached the end", expect_user_response=True))
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=9000)
+    app.run(host="0.0.0.0", port=9000, debug=False)
+
