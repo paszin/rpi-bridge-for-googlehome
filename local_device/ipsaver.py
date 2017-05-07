c9c7f29a7e859edaa8eec32f41abda0e47cf02fc
paszin <pascalslogin@gmail.com>
2017-05-08 00:20:17 +0200

    updated ipsaver

--- local_device/ipsaver.py
+++ local_device/ipsaver.py
@@ -2,32 +2,48 @@
 ## every 60 seconds the script will check your ip address and sends it to the webservice if necessary
 ## Requirements:
 ##  pip install requests
-##  put the url of your webservice in webUrl = ...
+##  put the url of your webservice in MYSMARTHOME_APIURL = ...
+##  or export it to the enviroment variable MYSMARTHOME_APIURL=....
 
-
-
-
+import os
+import sys
 import urllib2
 import json
 import time
 import requests
 
 
-url = "https://api.ipify.org/?format=json"
-webUrl = "https://???????.execute-api.eu-central-1.amazonaws.com/prod"
+
+MYSMARTHOME_APIURL = "https://???????.execute-api.eu-central-1.amazonaws.com/dev/"
+MYSMARTHOME_DEVICEID = "lights"
+MYSMARTHOME_PORT = "9000"
+
+webUrl = os.environ.get("MYSMARTHOME_APIURL") or MYSMARTHOME_APIURL
+deviceid = os.environ.get("MYSMARTHOME_DEVICEID") or MYSMARTHOME_DEVICEID
+port = os.environ.get("MYSMARTHOME_PORT") or MYSMARTHOME_PORT
+endpoint = "/" + deviceid
 
 interval = 60 ##seceonds
 
+url = "https://api.ipify.org/?format=json"
+
 lastIp = None
 
-
 while True:
-    
+
+    ##get ip
     response = urllib2.urlopen(url)
-    data = json.load(response)   
+    data = json.load(response)
     if data["ip"] != lastIp:
-        r = requests.put(webUrl, data=json.dumps({"ip": data["ip"]}))
-        print "updated ip"
+        r = requests.put(webUrl + endpoint, \
+         data=json.dumps({"ip": data["ip"], "port": port}))
+        if r.status != "200":
+            print "[Error]", r.text
+            break
+        print "new ip", data["ip"]
         lastIp = data["ip"]
 
+    if sys.argv[1] == "noloop":
+        break
+        
     time.sleep(interval)
